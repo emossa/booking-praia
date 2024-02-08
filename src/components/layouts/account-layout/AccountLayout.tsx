a908755d08fd331beb9239736de8e484e8a3e321
Emanuele Mossa <emanuelemossa@svcli100d.originenet.it>
2024-02-08 20:37:49 +0100

    fix

--- src/components/layouts/account-layout/AccountLayout.tsx
+++ src/components/layouts/account-layout/AccountLayout.tsx
@@ -1,6 +1,5 @@
 import { Box, Spinner } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { useAccountLayoutStore } from "./store";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -9,7 +8,6 @@ const AccountLayout: React.FC<{ children: React.ReactNode; roles: '' }> = ({
   roles,
 }) => {
   const { data } = useSession({ required: true });
-  const isLoading = useAccountLayoutStore((state: any) => state.isLoading);
   const router = useRouter();
 
   const [width, setWidth] = useState<number>(0);
@@ -28,20 +26,8 @@ const AccountLayout: React.FC<{ children: React.ReactNode; roles: '' }> = ({
 
   return (
     <main className="flex justify-between">
-      {isLoading && (
-        <Box
-          position={"fixed"}
-          top={0}
-          width="full"
-          height="full"
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          zIndex={2}
-        >
-          <Spinner size="xl" />
-        </Box>
-      )}
+
+
       {children}
     </main>
   );
--- src/components/layouts/account-layout/store.ts
+++ src/components/layouts/account-layout/store.ts
@@ -1,10 +1,4 @@
-import { create } from "zustand";
-
 interface AccountLayoutStore {
   isLoading: boolean;
   setIsLoading: (isLoading: boolean) => void;
 }
-export const useAccountLayoutStore = create<AccountLayoutStore>((set) => ({
-  isLoading: false,
-  setIsLoading: (isLoading) => set({ isLoading }),
-}));
